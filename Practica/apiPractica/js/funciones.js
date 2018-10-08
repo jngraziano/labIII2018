8f2f525520f9045a2b8f19f9b83dda8d35197085
Julian Graziano <jgraziano@medel.com>
2018-10-08 17:52:02 -0300

    Falta que funcionen correcto boton edit y eliminar, el resto OK

--- Practica/apiPractica/js/funciones.js
+++ Practica/apiPractica/js/funciones.js
@@ -10,16 +10,20 @@ window.onload = function () {
     xml.send();
 };
 
+// CALLBACK GET Y POST
 
 function callBackGET() {
+    //verifico que tenga una respuesta correcta del servidor
     if (xml.readyState == 4) {
-        //verifico que tenga una respuesta correcta. 
+    //verifico que sea 200 el status que es el correcto
         if (xml.status == 200) {
+
             // JSON.parse y JSON.stringify para pasar de string a jsonObject y viceversa
             // var jsonString = JSON.stringify({"edad":20});
             // Lo muestro en el label del html
             // document.getElementById("textoLabel").innerHTML = xml.responseText;
             // puedo pasar el texto a un JSON para poder acceder 
+
             var noticias = JSON.parse(xml.responseText);
             var cuerpo = document.getElementById('tCuerpo');
             var seccionNoticias ="";
@@ -29,31 +33,94 @@ function callBackGET() {
              seccionNoticias+=    "<tr><td><p id='temaNoti'><small>"+noticias[i].tema+"</small></p>" +
                                   "<h3 id='tituloNoti'>"+noticias[i].titulo  + "</h3>"+
                                   "<p id='noticiaNoti'>"+noticias[i].noticia + "</p>" +
-                                  "<button class='btn btn-info' id='btnResultado' onclick='btnResultado()'>" +
+                                  "<button class='btn btn-info' id='btnResultado' onclick='btnEdit()'>" +
                                   "<i class='glyphicon glyphicon-pencil'></i></button>" +
-                                  "<button class='btn btn-danger' id='btnResultado' onclick='btnResultado()'>" +
-                                  "<i class='glyphicon glyphicon-remove'></i></button></td></tr>";
+                                  "<button class='btn btn-danger' id='btnResultado' onclick='btnBorrar()'>" +
+                                  "<i class='glyphicon glyphicon-remove'></i></button></td></tr>"+
+                                  "<input type='hidden' id='idNoti' value='"+noticias[i].id+"'>";
 
             cuerpo.innerHTML = seccionNoticias;
                 
-
-            // document.getElementById("titulo1").innerHTML = noticias[0].titulo;
-            // document.getElementById("noticia1").innerHTML = noticias[0].noticia;
-            // document.getElementById("titulo2").innerHTML = noticias[1].titulo;
-            // document.getElementById("noticia2").innerHTML = noticias[1].noticia;
-            // document.getElementById("titulo3").innerHTML = noticias[2].titulo;
-            // document.getElementById("noticia3").innerHTML = noticias[2].noticia;
              }
              console.log(noticias);
         }
     }
 }
 
-function edicionClick() {
-    document.getElementById("add-edit").hidden = false;
+
+
+
+
+function callBackPOST() {
+    if (xml.readyState == 4) {
+        //verifico que tenga una respuesta correcta. 
+        if (xml.status == 200) {
+            alert("Realizado.");
+            location.reload();
+
+        }
+    }
 }
-function btnCerrar() {
-    document.getElementById("add-edit").hidden = true;
+
+function callbackMODIF() {
+   
+
+    if (xml.readyState == 4) {
+        
+            if (xml.status == 200) {
+
+               
+
+                var noticias = JSON.parse(xml.responseText);
+
+                var idHidden =document.getElementById("idNoti").value;
+
+               
+
+                // for(var i=0;i<noticias.length;i++)
+                // {
+                //     if (idHidden == noticias[i]) {
+                //         document.getElementById("add-edit").hidden = false;
+                //         document.getElementById("tituloE").value= noticias[i].titulo;
+                //         // document.getElementById("tipoTema").
+                //         document.getElementById("descipcionE").value= noticias[i].noticia;
+                //         this.btnAgregar();
+                        
+                //     }
+                //     else
+                //     {
+                //         alert("elemento no encontrado.");
+                //     }
+
+                // }
+
+
+
+                var cuerpo = document.getElementById('tCuerpo');
+                var seccionNoticias ="";
+    
+                 
+                 console.log(noticias);
+            }
+        }
+}
+
+
+// BOTONES
+
+
+function btnEdit() {
+
+    xml.onreadystatechange = callbackMODIF;
+
+    xml.open("POST", "http://localhost:3000/editarNoticia", true);
+
+
+
+    xml.send();
+
+
+    
 }
 
 function btnAgregar() {
@@ -66,9 +133,32 @@ function btnAgregar() {
 
     var titulo = document.getElementById("tituloE").value;
     var temaNum = document.getElementById("tipoTema").value;
-    var tema = "";
+    var tema = this.stringTema(temaNum);
+
+
+    var descripcion = document.getElementById("descripcionE").value;
+    var email ="add@add.com";
+
+    var datos ={"titulo":titulo,"tema":tema,"noticia":descripcion,"email":email};
+
+    xml.send(JSON.stringify(datos));
     
-    switch (temaNum) {
+}
+
+function edicionClick() {
+    document.getElementById("add-edit").hidden = false;
+}
+function btnCerrar() {
+    document.getElementById("add-edit").hidden = true;
+}
+
+
+
+//Tranformar Select/Option a string
+
+function stringTema(number) {
+
+    switch (number) {
         case "0":
         tema="Deportes";
         break;
@@ -82,22 +172,6 @@ function btnAgregar() {
             break;
     }
 
-    var descripcion = document.getElementById("descripcionE").value;
-    var email ="add@add.com";
-
-    var datos ={"titulo":titulo,"tema":tema,"noticia":descripcion,"email":email};
-
-    xml.send(JSON.stringify(datos));
+    return number;
     
-}
-
-function callBackPOST() {
-    if (xml.readyState == 4) {
-        //verifico que tenga una respuesta correcta. 
-        if (xml.status == 200) {
-            alert("Usuario cargado");
-            location.reload();
-
-        }
-    }
 }
