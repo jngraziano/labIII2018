75f8f71599343de5f7027a451ee6e1e60cb888fc
Julian Graziano <jgraziano@medel.com>
2018-11-28 17:49:56 -0300

    mostraranimales()

--- Practica/practicaSP/clases/app.js
+++ Practica/practicaSP/clases/app.js
@@ -1,4 +1,3 @@
-"use strict";
 var practicaSP;
 (function (practicaSP) {
     $(document).ready(function () {
@@ -9,6 +8,7 @@ var practicaSP;
         $("#btnCerrar").click(function () {
             $("#divOculto1").css("visibility", "hidden");
         });
+        mostrarAnimales();
         $("#radGatoA").on("click", function () {
             $("#radPerroA").prop("checked", false);
         });
@@ -16,7 +16,6 @@ var practicaSP;
             $("#radGatoA").prop("checked", false);
         });
         $("#btnAgregar").click(function () {
-            //$("btn");
             // window.location.href="./index2.html"; //Tomo otro html
             var nombreA = String($("#nombreA").val());
             var patasA = Number($("#patasA").val());
@@ -24,11 +23,11 @@ var practicaSP;
             var radPerroA = $("#radPerroA").prop("checked");
             var animales = new Array();
             if (radGatoA) {
-                var unGato = new practicaSP.gato(nombreA, patasA);
+                var unGato = new gato(nombreA, patasA);
                 animales.push(unGato);
             }
             else if (radPerroA) {
-                var unPerro = new practicaSP.perro(nombreA, patasA);
+                var unPerro = new perro(nombreA, patasA);
                 animales.push(unPerro);
             }
             //    animales.forEach(Programa.hablar);
@@ -36,7 +35,34 @@ var practicaSP;
             localStorage.setItem("key", jsonAnimales);
             //    let unJson = JSON.parse(localStorage.getItem("key")); //me dijo el profesor
             //    alert(unJson[0]);
-            alert(localStorage.getItem("key"));
+            //    alert(localStorage.getItem("key"));
         });
     }); //fin document.ready
+    function mostrarAnimales() {
+        var animalesString = JSON.parse(localStorage.getItem("key") || "[]");
+        // let stringFinal = animalesString
+        //                         .filter(function(empleado){
+        //                             let empleadoRet = JSON.parse(empleado);
+        //                             return empleadoRet._tipo == valor;
+        //                         })
+        //                         .map(function(empleado){
+        //                             let empleadoRet = JSON.parse(empleado);
+        //                             return empleadoRet;
+        //                         });   
+        // EmpleadosString= stringFinal;
+        var tBodyTable = $('#tBodyTable')[0];
+        var seccionPersonas = "";
+        // for (let i = 0; i < animalesString.length; i++) {
+        //     alert(animalesString[i]);
+        // seccionPersonas += "<tr>  <td hidden>"+ personasCompleto[i].id       + "</td>" +
+        //                               "<td>" +      personasCompleto[i].nombre   + "</td>" +
+        //                               "<td>" +      personasCompleto[i].apellido + "</td>" +
+        //                               "<td>" +      personasCompleto[i].fecha    + "</td>" +
+        //                               "<td>" +      personasCompleto[i].sexo     + "</td>"+
+        //                               "<td>" + "<img src='"+ personasCompleto[i].foto + "'id='imgMuestro' height='80'>"+
+        //                               "<input type='file' "+" hidden>"+"</td>"+
+        //                      "</tr>" ;
+        // tBodyTable.innerHTML = seccionPersonas;
+        // }
+    }
 })(practicaSP || (practicaSP = {}));
--- Practica/practicaSP/clases/app.ts
+++ Practica/practicaSP/clases/app.ts
@@ -5,15 +5,18 @@ namespace practicaSP{
 
         $("#addPopup").click(function () {
         // $("#divOculto1").show(1000);
-        $("#divOculto1").css("visibility","visible");
-        
-            
+        $("#divOculto1").css("visibility","visible"); 
         });
 
         $("#btnCerrar").click(function () {
         $("#divOculto1").css("visibility","hidden");
          
         });
+
+
+        mostrarAnimales();
+
+
 
         $("#radGatoA").on("click",function () {
             $("#radPerroA").prop("checked",false);
@@ -27,8 +30,7 @@ namespace practicaSP{
 
         $("#btnAgregar").click(function () {
             
-            //$("btn");
-           
+                      
 
            // window.location.href="./index2.html"; //Tomo otro html
            let nombreA:string = String($("#nombreA").val());
@@ -55,12 +57,49 @@ namespace practicaSP{
            localStorage.setItem("key", jsonAnimales);
         //    let unJson = JSON.parse(localStorage.getItem("key")); //me dijo el profesor
         //    alert(unJson[0]);
-           alert(localStorage.getItem("key"));
+        //    alert(localStorage.getItem("key"));
         })
        
         
     
     
     });//fin document.ready
+
+
+    function mostrarAnimales():void {
+        let animalesString:string|null =  JSON.parse(localStorage.getItem("key") || "[]");    
+    
+        // let stringFinal = animalesString
+        //                         .filter(function(empleado){
+        //                             let empleadoRet = JSON.parse(empleado);
+        //                             return empleadoRet._tipo == valor;
+        //                         })
+        //                         .map(function(empleado){
+        //                             let empleadoRet = JSON.parse(empleado);
+        //                             return empleadoRet;
+        //                         });   
+        // EmpleadosString= stringFinal;
+        var tBodyTable = $('#tBodyTable')[0];
+        var seccionPersonas:string = "";
+
+        // for (let i = 0; i < animalesString.length; i++) {
+        //     alert(animalesString[i]);
+            // seccionPersonas += "<tr>  <td hidden>"+ personasCompleto[i].id       + "</td>" +
+            //                               "<td>" +      personasCompleto[i].nombre   + "</td>" +
+            //                               "<td>" +      personasCompleto[i].apellido + "</td>" +
+            //                               "<td>" +      personasCompleto[i].fecha    + "</td>" +
+            //                               "<td>" +      personasCompleto[i].sexo     + "</td>"+
+            //                               "<td>" + "<img src='"+ personasCompleto[i].foto + "'id='imgMuestro' height='80'>"+
+            //                               "<input type='file' "+" hidden>"+"</td>"+
+
+                                    
+                                         
+
+            //                      "</tr>" ;
+            // tBodyTable.innerHTML = seccionPersonas;
+
+    // }
+}
+
 }
 
