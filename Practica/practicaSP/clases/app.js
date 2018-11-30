2ed4dc106e834486b3cd6fdeafdb6a7be6bdaaba
jngraziano <jngraziano@gmail.com>
2018-11-30 18:33:53 -0300

    Se hizo lo que se pudo

--- Practica/practicaSP/clases/app.js
+++ Practica/practicaSP/clases/app.js
@@ -4,11 +4,12 @@ var practicaSP;
     var usuarioID;
     $(document).ready(function () {
         $("#addPopup").click(function () {
-            // $("#divOculto1").show(1000);
             $("#btnModificar").css("visibility", "hidden");
             $("#btnEliminar").css("visibility", "hidden");
             $("#btnAgregar").css("visibility", "visible");
             $("#divTipo").css("visibility", "visible");
+            $('#nombreA').attr('value', "");
+            $('#sondioA').attr('value', "");
         });
         $("#btnCerrar").click(function () {
             $("#divOculto1").css("visibility", "hidden");
@@ -30,6 +31,35 @@ var practicaSP;
             $("#radGatoA").prop("checked", false);
             $("#radPerroA").prop("checked", false);
         });
+        $("#checkFORM :checkbox").on("click", function () {
+            var checkboxON = $('input:checkbox:checked.checkItems').map(function () { return this.value; }).get();
+            if (checkboxON.length == 0) {
+                location.reload();
+            }
+            checkboxON.includes("nombreF") == true ? $(".col1").css("display", "none") : null;
+            checkboxON.includes("numPatasF") == true ? $(".col2").css("display", "none") : null;
+            checkboxON.includes("tipoF") == true ? $(".col3").css("display", "none") : null;
+            checkboxON.includes("sonidoF") == true ? $(".col4").css("display", "none") : null;
+        });
+        $("#btnPrmedio").click(function () {
+            soluciones.promedio = function (usuarios) {
+                var acumEdad = usuarios
+                    .reduce(function (actual, siguiente) {
+                    return actual + siguiente.edad;
+                }, 0); //Inicializa y arranca en 0 porque es un numero lo que devuelve y no un objeto
+                //En la primer iteracion, actual toma 0 como valor
+                var cantidad = usuarios
+                    .reduce(function (actual, siguiente) {
+                    return actual + 1;
+                }, 0);
+                return (acumEdad / cantidad).toFixed(2);
+            };
+        });
+        $("#filtrarPor").change(function () {
+            var valorFiltro = $('#filtrarPor').map(function () { return this.value; }).get();
+            mostrarAnimales(valorFiltro);
+            // tablaAux = undefined;
+        });
         $("#tablaID tbody tr").dblclick(function (e) {
             $('#myModal').modal('show');
             $("#btnModificar").css("visibility", "visible");
@@ -41,8 +71,8 @@ var practicaSP;
             var tipoSelec = $(e.target).next().next().text();
             var ruidoSelec = $(e.target).next().next().next().text();
             $("#divTipo").css("visibility", "hidden");
-            // $("#nombreA").text() = nombreSelec;
-            // $("#sondioA").text() = ruidoSelec;
+            $('#nombreA').attr('value', nombreSelec);
+            $('#sondioA').attr('value', ruidoSelec);
             // let fila = $(target).parent();
             // let celdas = fila.$("td");
             // usuarioID= celdas[0].innerHTML;    
@@ -89,8 +119,10 @@ var practicaSP;
             //    alert(localStorage.getItem("animales"));
         });
     }); //fin document.ready
-    function mostrarAnimales() {
+    function mostrarAnimales(valor) {
         var animalesStorage = JSON.parse(localStorage.getItem("Localanimal") || "[]");
+        var tBodyTable = $('#tBodyTable')[0];
+        var seccionPersonas = "";
         // let stringFinal = animalesString
         //                         .filter(function(empleado){
         //                             let empleadoRet = JSON.parse(empleado);
@@ -101,15 +133,37 @@ var practicaSP;
         //                             return empleadoRet;
         //                         });   
         // EmpleadosString= stringFinal;
-        var tBodyTable = $('#tBodyTable')[0];
-        var seccionPersonas = "";
+        if (valor) {
+            //MUESTRO EL LISTADO DE EmpleadoS SEGUN FILTRO
+            var stringFinal = animalesStorage
+                .filter(function (animal) {
+                var animalRet = JSON.parse(animal);
+                return animalRet.tipo == valor;
+            })
+                .map(function (animal) {
+                var animalRet = JSON.parse(animal);
+                return animalRet;
+            });
+            animalesStorage = stringFinal;
+        }
+        // $("#optionNull").
+        if ($('#filtrarPor').val() == "todos") {
+            location.reload();
+        }
         for (var i = 0; i < animalesStorage.length; i++) {
-            var animalActual = JSON.parse(animalesStorage[i]);
+            // let animalActual = JSON.parse(animalesStorage[i]);
+            var animalActual = void 0;
+            if (valor) {
+                animalActual = animalesStorage[i];
+            }
+            else {
+                animalActual = JSON.parse(animalesStorage[i]);
+            }
             seccionPersonas += "<tr>      <td hidden>" + animalActual.ID + "</td>" +
-                "<td>" + animalActual.nombre + "</td>" +
-                "<td>" + animalActual.cantPatas + "</td>" +
-                "<td>" + animalActual.tipo + "</td>" +
-                "<td>" + animalActual.ruido + "</td>" +
+                "<td class='col1'>" + animalActual.nombre + "</td>" +
+                "<td class='col2'>" + animalActual.cantPatas + "</td>" +
+                "<td class='col3'>" + animalActual.tipo + "</td>" +
+                "<td class='col4'>" + animalActual.ruido + "</td>" +
                 //   "<td>" +      personasCompleto[i].apellido + "</td>" +
                 //   "<td>" +      personasCompleto[i].fecha    + "</td>" +
                 //   "<td>" +      personasCompleto[i].sexo     + "</td>"+
@@ -120,5 +174,20 @@ var practicaSP;
                 "</tr>";
             tBodyTable.innerHTML = seccionPersonas;
         }
+    }
+    function eliminarAnimal(idAnimal) {
+        var indice = determinoIndice(idAnimal);
+        modificarEmpleado(indice, Clases.estadoCLIEMP.BAJA);
+    }
+    function determinoIndice(idEmpleado) {
+        var retorno;
+        var animalesStorage = JSON.parse(localStorage.getItem("Localanimal") || "[]");
+        for (var i = 0; i < animalesStorage.length; i++) {
+            var empleadoActual = JSON.parse(animalesStorage[i]);
+            if (empleadoActual._id == idEmpleado) {
+                retorno = i;
+            }
+        }
+        return retorno;
     }
 })(practicaSP || (practicaSP = {}));
--- Practica/practicaSP/clases/app.ts
+++ Practica/practicaSP/clases/app.ts
@@ -6,11 +6,13 @@ namespace practicaSP{
         
 
         $("#addPopup").click(function () {
-        // $("#divOculto1").show(1000);
+      
         $("#btnModificar").css("visibility","hidden");
         $("#btnEliminar").css("visibility","hidden");
         $("#btnAgregar").css("visibility","visible");
         $("#divTipo").css("visibility","visible");
+        $('#nombreA').attr('value', "");
+        $('#sondioA').attr('value', "");
 
 
         });
@@ -47,6 +49,51 @@ namespace practicaSP{
             
         });
 
+        $("#checkFORM :checkbox").on("click",function() {
+
+            var checkboxON = $('input:checkbox:checked.checkItems').map(function() { return this.value; }).get();
+
+          
+            if(checkboxON.length == 0)         {  location.reload();}
+            checkboxON.includes("nombreF")       == true ? $(".col1").css("display","none"):null;
+            checkboxON.includes("numPatasF")       == true ? $(".col2").css("display","none"):null;
+            checkboxON.includes("tipoF")       == true ? $(".col3").css("display","none"):null;
+            checkboxON.includes("sonidoF")       == true ? $(".col4").css("display","none"):null;
+
+        
+
+            
+
+        });
+
+
+
+        
+
+        $("#btnPrmedio").click(function () {
+
+                soluciones.promedio = function(usuarios){
+                var acumEdad = usuarios
+                .reduce(function(actual,siguiente){
+                    return actual+siguiente.edad;
+                },0); //Inicializa y arranca en 0 porque es un numero lo que devuelve y no un objeto
+                //En la primer iteracion, actual toma 0 como valor
+            
+                var cantidad = usuarios
+                .reduce (function(actual,siguiente){
+                    return actual + 1;
+                }, 0);
+                return (acumEdad / cantidad).toFixed(2);
+            }
+            
+        });
+
+        $("#filtrarPor").change(function(){
+            let valorFiltro = $('#filtrarPor').map(function() { return this.value; }).get();
+            mostrarAnimales(valorFiltro);
+            // tablaAux = undefined;
+        });
+
         $("#tablaID tbody tr").dblclick(function (e) {
             
         
@@ -68,14 +115,11 @@ namespace practicaSP{
 
            $("#divTipo").css("visibility","hidden");
 
+           $('#nombreA').attr('value', nombreSelec);
+           $('#sondioA').attr('value', ruidoSelec);
 
-            // $("#nombreA").text() = nombreSelec;
-            // $("#sondioA").text() = ruidoSelec;
-
-
-
-
-
+         
+            
 
 
             // let fila = $(target).parent();
@@ -123,7 +167,7 @@ namespace practicaSP{
 
            let radGatoA:boolean = $("#radGatoA").prop("checked");
            let radPerroA:boolean = $("#radPerroA").prop("checked");
-        //    let radPajaroA:boolean = $("#radPajaroA").prop("checked");
+           //    let radPajaroA:boolean = $("#radPajaroA").prop("checked");
 
 
             let animalesLista = JSON.parse(localStorage.getItem("Localanimal") || "[]");
@@ -164,8 +208,10 @@ namespace practicaSP{
 
     
 
-    function mostrarAnimales():void {
-        let animalesStorage:any|null =  JSON.parse(localStorage.getItem("Localanimal") || "[]");    
+    function mostrarAnimales(valor?):void {
+        let animalesStorage:any|null =  JSON.parse(localStorage.getItem("Localanimal") || "[]"); 
+        var tBodyTable = $('#tBodyTable')[0];
+        var seccionPersonas:string = "";   
     
         // let stringFinal = animalesString
         //                         .filter(function(empleado){
@@ -177,20 +223,42 @@ namespace practicaSP{
         //                             return empleadoRet;
         //                         });   
         // EmpleadosString= stringFinal;
-        var tBodyTable = $('#tBodyTable')[0];
-        var seccionPersonas:string = "";
+       
 
-        
-
+        if(valor)
+        {
+        //MUESTRO EL LISTADO DE EmpleadoS SEGUN FILTRO
+           let stringFinal = animalesStorage
+                                    .filter(function(animal:any){
+                                        let animalRet = JSON.parse(animal);
+                                        return animalRet.tipo == valor;
+                                    })
+                                    .map(function(animal:any){
+                                        let animalRet = JSON.parse(animal);
+                                        return animalRet;
+                                    });   
+                        animalesStorage= stringFinal;
+        }
+       
+        // $("#optionNull").
+        if($('#filtrarPor').val() == "todos")
+        {
+            location.reload();
+            
+        }
         for (let i = 0; i < animalesStorage.length; i++) {
             
-            let animalActual = JSON.parse(animalesStorage[i]);
+            // let animalActual = JSON.parse(animalesStorage[i]);
+
+            let animalActual;
+            if(valor){animalActual = animalesStorage[i];}
+            else     {animalActual = JSON.parse(animalesStorage[i]);}
 
             seccionPersonas += "<tr>      <td hidden>"+ animalActual.ID   + "</td>" + 
-                                          "<td>" +      animalActual.nombre      + "</td>" +
-                                          "<td>" +      animalActual.cantPatas   + "</td>" +
-                                          "<td>" +      animalActual.tipo  + "</td>" +
-                                          "<td>" +      animalActual.ruido  + "</td>" +
+                                          "<td class='col1'>" +      animalActual.nombre      + "</td>" +
+                                          "<td class='col2'>" +      animalActual.cantPatas   + "</td>" +
+                                          "<td class='col3'>" +      animalActual.tipo  + "</td>" +
+                                          "<td class='col4'>" +      animalActual.ruido  + "</td>" +
                                         //   "<td>" +      personasCompleto[i].apellido + "</td>" +
                                         //   "<td>" +      personasCompleto[i].fecha    + "</td>" +
                                         //   "<td>" +      personasCompleto[i].sexo     + "</td>"+
@@ -202,6 +270,25 @@ namespace practicaSP{
             tBodyTable.innerHTML = seccionPersonas;
 
     }
+}
+
+function eliminarAnimal(idAnimal:number):void
+{
+    var indice = determinoIndice(idAnimal);
+    modificarEmpleado(indice,Clases.estadoCLIEMP.BAJA);
+} 
+
+function determinoIndice (idEmpleado:number)
+{
+    var retorno;
+    let animalesStorage:string|null =  JSON.parse(localStorage.getItem("Localanimal") || "[]"); 
+    for (var i = 0; i < animalesStorage.length ; i++) 
+    {
+       let empleadoActual = JSON.parse(animalesStorage[i]);
+       if (empleadoActual._id == idEmpleado)
+       {retorno = i;}
+    }
+    return retorno;
 }
 
 }
